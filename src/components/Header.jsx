2a2439e92339f010dce87f33c4430ca817dbfa2d
Arsemawit28 <mekonnenarsemawit@gmail.com>
2026-06-24 16:07:53 +0300

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>Theatre Arts Students</h1>
+        <p className="tagline">Class of 2026 - Casting Portfolio</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
